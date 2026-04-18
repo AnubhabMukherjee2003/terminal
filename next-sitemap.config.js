8f2a78b719d8e8428137f74d641532e77bb042b4
AnubhabMukherjee2003 <antucruse621@gmail.com>
2026-04-18 12:18:50 +0530

    sitemap change 3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,5 @@
 // next-sitemap.config.js
-const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://anubhabmukherjee-terminal.vercel.app');
+const siteUrl = 'https://anubhabmukherjee-terminal.vercel.app'
 
 module.exports = {
   siteUrl,
